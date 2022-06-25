27f9ee84b51374b62a12f5daf67ba6d5ced300f4
AlexanderSamisko <83254509+AlexanderSamisko@users.noreply.github.com>
2022-06-25 13:28:11 +0400

    second attempt

--- e2e/Validator.test.js
+++ e2e/Validator.test.js
@@ -54,4 +54,19 @@ describe('Credit Card Validator form', () => {
     const value = await page.evaluate((el) => el.textContent, hint);
     expect(value).toBe('This card is Valid');
   });
+
+  test('should add do something', async () => {
+    new Widget();
+    Validator.Validate('widget-card-input');
+    await page.goto(baseUrl);
+    const input = await page.$('.widget-card-input');
+    await input.type('6011991626216936');
+    const button = await page.$('.widget-card-input-button');
+    await button.click();
+
+    await page.waitForSelector('.widget-hint');
+    const hint = await page.$('.widget-hint');
+    const value = await page.evaluate((el) => el.textContent, hint);
+    expect(value).toBe('This card is invalid');
+  });
 });
